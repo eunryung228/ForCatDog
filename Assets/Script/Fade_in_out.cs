8578fc452ca69f73d94be6f1b33d73adc6b24abe
eunryung228 <ryaner@khu.ac.kr>
2020-07-25 14:47:12 +0900

    add TitleScene from dw

---
+++ Assets/Script/Fade_in_out.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade_in_out : MonoBehaviour
+{
+    public Image Panel;
+    public Image TitleImage;
+    public Image Mainlogo;
+    float time = 0;
+    float F_time = 1f;
+
+    public void Fade()
+    {
+        StartCoroutine(FadeFlow());
+    }
+    IEnumerator FadeFlow()
+    {
+        Panel.gameObject.SetActive(true);
+        time = 0f;
+        Color alpha = Panel.color;
+
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+
+        time = 0f;
+        yield return new WaitForSeconds(1f);
+        Mainlogo.gameObject.SetActive(false);
+
+        while (alpha.a > 0f)
+        {       
+            TitleImage.gameObject.SetActive(true);
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        Panel.gameObject.SetActive(false);
+        yield return null;
+    }
+}
---
+++ Assets/Script/MoveScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    public void Click_Move_Main_Scene() // 은령: 수정을 조금 했습니다!
+    {
+        SceneManager.LoadScene(1);
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+    }
+}
