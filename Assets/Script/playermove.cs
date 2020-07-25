8f551b157e1389cc3fd183c97c6c83fffbcc8b63
eunryung228 <ryaner@khu.ac.kr>
2020-07-25 20:37:14 +0900

    add playerMove

---
+++ Assets/Script/playermove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    public float Speed;
+
+    Rigidbody2D rigid;
+    Animator anim;
+    float h;
+    float v;
+    bool isHorizonMove;
+    
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //move 값
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //버튼 다운 & 업
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown || vUp)
+            isHorizonMove = true;
+
+        else if (vDown || hUp)
+            isHorizonMove = false;
+
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if(anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            anim.SetBool("isChange", false);
+
+
+    }
+    void FixedUpdate() //move
+    {
+        Vector2 moveVex = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVex * Speed;
+    }
+}
